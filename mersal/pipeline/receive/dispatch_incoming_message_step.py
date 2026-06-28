61f1f7abfdd26420587347dffece88e4b133e7ad
Abdulhaq Emhemmed <abdulhaq.emhemmed@gmail.com>
2026-06-28 13:36:55 +0200

    chore: Clarify message that wasn't dispatched

--- mersal/pipeline/receive/dispatch_incoming_message_step.py
+++ mersal/pipeline/receive/dispatch_incoming_message_step.py
@@ -1,3 +1,4 @@
+from mersal.messages import LogicalMessage
 from mersal.pipeline.incoming_step import IncomingStep
 from mersal.pipeline.incoming_step_context import IncomingStepContext
 from mersal.pipeline.receive.handler_invokers import HandlerInvokers
@@ -9,9 +10,12 @@ __all__ = ("DispatchIncomingMessageStep",)
 class DispatchIncomingMessageStep(IncomingStep):
     async def __call__(self, context: IncomingStepContext, next_step: AsyncAnyCallable) -> None:
         invokers: HandlerInvokers = context.load(HandlerInvokers)
-
+        logical_message = context.load(LogicalMessage)
+        message = logical_message.body
         if not invokers:
-            raise Exception("Message was not dispatched to any handlers")
+            raise Exception(
+                f"Message {type(message)}/{logical_message.message_label} was not dispatched to any handlers"
+            )
 
         for invoker in invokers:
             await invoker()
